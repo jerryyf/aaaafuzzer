c4aa42b3a571165777c531434bbaa2f1a7e75c0e
Yijin Du <yijindu20@gmail.com>
2023-10-25 23:57:02 +1100

    fix checktype for csv

--- src/checkType.py
+++ src/checkType.py
@@ -10,6 +10,7 @@ Returns: True   - If the file type match
          False  - Otherwise
 """
 def checkTypeJson(sample_binary):
+    sample_binary.seek(0)
 
     # Read file content from begining, try load by JSON
     try:
@@ -22,6 +23,7 @@ def checkTypeJson(sample_binary):
     return True
 
 
+
 """
 Helpers for fuzzer, checks if the type of given input is in CSV format
 
@@ -31,18 +33,23 @@ Returns: True   - If the file type match
          False  - Otherwise
 """
 def checkTypeCSV(sample_binary):
+    sample_binary.seek(0)
 
-    # Read file content from begining, try load by CSV
-    try:
-        data = csv.reader(sample_binary, delimiter=',') # NOTE assumption that all csv files parsed are delimted with commas
-        for row in data:
-            if len(row) <= 1:                     # NOTE assumption that csv file contains more than 1 column
-                print('Not a CSV')
-                return False
-        csvfile.seek(0)
+    # Check type of input file by line and commas counting
+    lines = sample_binary.readlines()
 
-    except:
-    # If load fail, return false
+    commas = lines[0].count(",")
+
+    # if file has one line only or no commas, return false
+    if len(lines) == 1 or commas == 0:
+        print(f"Not CSV file...")
         return False
-        
+    
+    # compare comma numbers for each line, if not match then retuirn false
+    for l in lines:
+        if l.count(",") != commas:
+            print(f"Not CSV file...")
+            return False
+    
+    # type check passed, return true
     return True
--- src/fuzzer.py
+++ src/fuzzer.py
@@ -12,10 +12,6 @@ from checkType import *
 N_ITER = 10 # set this to a larger number of iterations later or get input
 LEN = 100
 
-# For test purpose on multiprocessing, feel free to delete
-def JsonFuzzer(sampleInput, binary_file, lock):
-    print(f"sucess")
-
 if __name__ == '__main__':
 
     # Check valid input
@@ -23,31 +19,21 @@ if __name__ == '__main__':
         print(f"Invalid input format! Usage: ./fuzzer binary input.txt")
         sys.exit(1)
 
-    # IF valid, then check given input file, extract
+    # If valid, then check given input file, extract
     try: 
         binary_input = open(sys.argv[2], 'r')
 
     except:
         print(f"Error: Cannot open .txt file")
         sys.exit(1)
-
-    binary_file = sys.argv[1]
-
-    # Record the time process start
+        
     init_time = time.time()
-
-    # Create a mission list
-    mission = []
-    lock = multiprocessing.Lock()
-
-    # Assumed cpu number is 5, maybe need to change later to maximise performance
-    executor = ThreadPoolExecutor()
+    binary_file = sys.argv[1]
 
     if checkTypeJson(binary_input):
         print(f"going to assess binary as JSON")
-        mission.append(executor.submit(JsonFuzzer, binary_input, binary_file, lock))
 
-    if checkTypeCSV(binary_input):  
+    elif checkTypeCSV(binary_input):  
         print(f"going to assess binary as CSV")
 
 
@@ -55,3 +41,24 @@ if __name__ == '__main__':
 
     print(f"Fuzzer process done in {final_time - init_time} seconds...")
 
+
+# Code for multiprocessing, keep for now
+################################################################
+# Record the time process start
+#
+# # Create a mission list
+# mission = []
+# lock = multiprocessing.Lock()
+
+# # Assumed cpu number is 5, maybe need to change later to maximise performance
+# executor = ThreadPoolExecutor()
+
+# # For test purpose on multiprocessing, feel free to delete
+# def JsonFuzzer(sampleInput, binary_file, lock):
+#     print(f"sucess")
+#
+# # Usage: 
+#     if checkTypeJson(binary_input):
+#         print(f"going to assess binary as JSON")
+#         mission.append(executor.submit(JsonFuzzer, binary_input, binary_file, lock))
+#         print(f"mission[]: {mission}")
