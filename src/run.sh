30d686a15030450bac56a2a1b654182b0b8ddce3
jerryyf <jerryyf@linsomnia.net>
2023-10-27 20:11:29 +1100

    add more runs

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,6 @@
+# test against provided binaries and sample inputs
+
 ./fuzzer.py ../binaries/json1 ../binaries/json1.txt
+./fuzzer.py ../binaries/json2 ../binaries/json2.txt
+# ./fuzzer.py ../binaries/csv1 ../binaries/csv1.txt
+# ./fuzzer.py ../binaries/csv2 ../binaries/csv2.txt
