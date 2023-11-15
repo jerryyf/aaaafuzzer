58bdb6fe4264c42fe7cf84c3dc449ce926bd9269
Yijin Du <yijindu20@gmail.com>
2023-11-15 14:39:33 +1100

    fix type check

--- src/checktype.py
+++ src/checktype.py
@@ -4,7 +4,7 @@ def check_type(sample_binary_str):
     cmd = ['file', sample_binary_str]
     ret = subprocess.run(cmd, stdout=subprocess.PIPE, text=True)
 
-    if 'CSV ASCII text' in ret.stdout:
+    if 'CSV ASCII text' or 'CSV text' in ret.stdout:
         return 'csv'
     elif 'JSON text data' in ret.stdout:
         return 'json'
