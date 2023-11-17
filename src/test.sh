9496ca1478c4a49ecf42d8bd93279d9dc9d6315a
jerryyf <jerryyf@linsomnia.net>
2023-11-17 15:26:32 +1100

    Restore all tests

--- src/test.sh
+++ src/test.sh
@@ -1,13 +1,13 @@
 # test against provided binaries and sample inputs
 
-# ./fuzzer ../assets/json1 ../assets/json1.txt
-# ./fuzzer ../assets/json2 ../assets/json2.txt
-# ./fuzzer ../assets/csv1 ../assets/csv1.txt
-# ./fuzzer ../assets/csv2 ../assets/csv2.txt
+./fuzzer ../assets/json1 ../assets/json1.txt
+./fuzzer ../assets/json2 ../assets/json2.txt
+./fuzzer ../assets/csv1 ../assets/csv1.txt
+./fuzzer ../assets/csv2 ../assets/csv2.txt
 
-# ./fuzzer ../assets/plaintext1 ../assets/plaintext1.txt
-# ./fuzzer ../assets/plaintext2 ../assets/plaintext2.txt
-# ./fuzzer ../assets/plaintext3 ../assets/plaintext3.txt
+./fuzzer ../assets/plaintext1 ../assets/plaintext1.txt
+./fuzzer ../assets/plaintext2 ../assets/plaintext2.txt
+./fuzzer ../assets/plaintext3 ../assets/plaintext3.txt
 
 ./fuzzer ../assets/test_binaries/testjson1 ../assets/json1.txt
 ./fuzzer ../assets/test_binaries/testjson2 ../assets/json1.txt
