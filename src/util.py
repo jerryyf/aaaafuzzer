01bd283c71d5ef417d75bfd256b7da686ccaf931
derek <dereksaejong@gmail.com>
2023-11-19 02:43:04 +1100

    Docstrings for util

--- src/util.py
+++ src/util.py
@@ -2,12 +2,23 @@ import sys
 import time
 import subprocess
 
+'''
+Prints invalid input format message and exits
+'''
 def usage():
     print(f"Invalid input format! Usage: ./fuzzer binary input.txt")
     sys.exit(1)
 
+'''
+Return: Time difference between current time and recorded time
+'''
 def curr_time_taken(init_time) -> float:
     return time.time() - init_time
 
+'''
+Spawn a process that runs a fuzzer using bad_input
+
+Return: Check value of the process
+'''
 def runfuzz(cmd, bad_input):
     return subprocess.run(cmd, input=bad_input, stdout=subprocess.PIPE, text=True)
