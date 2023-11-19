5773b4d08c31a0181550e0d06b3e68e3c0f7add6
jerryyf <jerryyf@linsomnia.net>
2023-11-19 17:50:25 +1100

    Fix xml parser error

--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import subprocess
+import xml
 
 def usage():
     print(f"Invalid input format! Usage: ./fuzzer binary input.txt")
@@ -10,4 +11,46 @@ def curr_time_taken(init_time) -> float:
     return time.time() - init_time
 
 def runfuzz(cmd, bad_input):
-    return subprocess.run(cmd, input=bad_input, stdout=subprocess.PIPE, text=True)
+    try:
+        result = subprocess.run(cmd, input=bad_input, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        return result
+    except subprocess.CalledProcessError as e:
+        pass
+
+def runfuzzsingleoption(cmd, bad_input):
+    payload = ""
+    for all in bad_input[0]:
+        payload += f"{all}\n"
+
+    for all in bad_input[1:]:
+        payload += f"{all}"
+
+    try:
+        result = subprocess.run(cmd, input=payload, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        return result
+    except subprocess.CalledProcessError as e:
+        pass
+
+def runfuzzoptions(cmd, bad_input, OPTION):
+
+    try:
+        # if menu num more than 1, fuzz for menu
+        if OPTION == 1:
+            input = "\n".join(bad_input[0]) 
+            input += f"\n{bad_input[1]}\n"
+            input_lines = sum(1 for char in input if char == '\n')
+            if input_lines != len(bad_input):
+                return False
+            result = subprocess.run(cmd, input=input, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+            return result
+
+        print(bad_input[0])
+        input = "\n".join(bad_input[0])
+        input += f"\n{bad_input[1]}\n"
+        print(f"input: [{input}]")
+        result = subprocess.run(cmd, input=input, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        print(result)
+        return result
+
+    except subprocess.CalledProcessError as e:
+        pass
