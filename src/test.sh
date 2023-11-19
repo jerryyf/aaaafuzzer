f7f96d82dfcf7c18220d6b47b478f6a45f808f48
jerryyf <jerryyf@linsomnia.net>
2023-11-19 19:41:38 +1100

    Cleanup

--- src/test.sh
+++ src/test.sh
@@ -16,20 +16,20 @@
 ./fuzzer ../assets/test_binaries/testxml3 ../assets/xml1.txt
 ./fuzzer ../assets/test_binaries/testxml3 ../assets/xml1.txt
 
+# Test Binaries for plaintext
+./fuzzer ../assets/test_binaries/testplaintext1 ../assets/test_binaries/test1.txt
+./fuzzer ../assets/test_binaries/testplaintext2 ../assets/test_binaries/test2.txt
+./fuzzer ../assets/test_binaries/testplaintext3 ../assets/test_binaries/test3.txt
+
 #########################################
 # Provided binaries and sample inputs   #
 #########################################
 
 ./fuzzer ../assets/json1 ../assets/json1.txt
 ./fuzzer ../assets/json2 ../assets/json2.txt
+
 ./fuzzer ../assets/csv1 ../assets/csv1.txt
 ./fuzzer ../assets/csv2 ../assets/csv2.txt
-
-## added for coverage testing CHSNGE LATER
-./fuzzer ../assets/test_binaries/testplaintext1 ../assets/test_binaries/test1.txt
-./fuzzer ../assets/test_binaries/testplaintext2 ../assets/test_binaries/test2.txt
-./fuzzer ../assets/test_binaries/testplaintext3 ../assets/test_binaries/test3.txt
-
 
 ./fuzzer ../assets/plaintext1 ../assets/plaintext1.txt
 ./fuzzer ../assets/plaintext2 ../assets/plaintext2.txt
